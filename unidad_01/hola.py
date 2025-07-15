416f04cfa2ba921c1994db59bc0a3ac66f960a18
ernestoaguaysol-docente <ernestoaguaysol.docente@gmail.com>
2025-07-15 16:09:01 +0000

    Funciones concatenadas

--- unidad_01/hola.py
+++ unidad_01/hola.py
@@ -1,7 +1,8 @@
 # Pregunta al usuario por su nombre
-nombre = input("Cuál es tu nombre?")
+# Eliminar espacios en blanco y Capitalizar
+nombre = input("Cuál es tu nombre?").strip().title()
+
+# nombre = nombre
 
 # Imprime hola y nombre
-print("Hola, \"amigo\"")
-
-# print(nombre)
+print(f"Hola, {nombre}")
