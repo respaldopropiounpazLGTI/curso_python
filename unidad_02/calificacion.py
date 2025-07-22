348cab99c04be651ff5630e78a4a8927162eb69e
ernestoaguaysol-docente <ernestoaguaysol.docente@gmail.com>
2025-07-22 18:22:01 +0000

    calificación

---
+++ unidad_02/calificacion.py
@@ -0,0 +1,12 @@
+score = int(input("Puntuación: "))
+
+if score >= 90:
+    print("Calificaión A")
+elif score >= 80:
+    print("Calificaión B")
+elif score >= 70:
+    print("Calificaión C")
+elif score >= 60:
+    print("Calificaión D")
+else:
+    print("Calificaión F")
