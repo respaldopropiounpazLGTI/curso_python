c9014ab45fd42886ee833094a7bd6ff979242048
ernestoaguaysol-docente <ernestoaguaysol.docente@gmail.com>
2025-07-22 16:45:21 +0000

    solución einstein

---
+++ lecciones/lecciones_01/einstein/einstein.py
@@ -0,0 +1,10 @@
+def main():
+    masa = input("m: ")
+    numero = int(masa)
+    c = 300000000**2
+    E = (numero*c)
+
+    print("Número equevalente en Jeule: ", E)
+
+
+main()
---
+++ lecciones/lecciones_01/reproduccion/reproduccion.py
@@ -0,0 +1,7 @@
+# texto = "Hola que tal"
+# print(texto.replace(" ", "..."))
+
+input("Cuánto costó la comida?")
+input("Qué porcentaje te gustaría dejar de propina?")
+
+print("Dejar $7.50")
