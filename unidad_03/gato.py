193b26456a544d405abe9bbcaca843852eed6e13
ernestoaguaysol-docente <ernestoaguaysol.docente@gmail.com>
2025-07-29 18:19:36 +0000

    for

--- unidad_03/gato.py
+++ unidad_03/gato.py
@@ -1,6 +1,14 @@
-i = 0
-while i < 3:
-    print("iteración: ", i)
-    print("miau")
-    i += 1
+def main():
+    maullar(obtener_numero())
 
+def obtener_numero():
+    while True:
+        n = int(input("Cuál es n? "))
+        if n > 0:
+            return n
+
+def maullar(n):
+    for _ in range(n):
+        print("miau")
+
+main()
