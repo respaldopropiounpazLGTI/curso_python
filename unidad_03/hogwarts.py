889cf1d560d07f204275e04adb3a57a1479f021d
ernestoaguaysol-docente <ernestoaguaysol.docente@gmail.com>
2025-07-31 21:43:59 +0000

    diccionarios

--- unidad_03/hogwarts.py
+++ unidad_03/hogwarts.py
@@ -1,10 +1,9 @@
-estudiantes = {
-    # Clave  ----- > Valor
-    "Hermione": "Gryffindor",
-    "Harry": "Gryffindor",
-    "Ron": "Gryffindor",
-    "Draco": "Slytherin",
-}
+estudiantes = [
+ {"nombre": "Hermione", "casa": "Gryffindor", "patronus": "Nutria"},
+ {"nombre": "Harry", "casa": "Gryffindor", "patronus": "Ciervo"},
+ {"nombre": "Ron", "casa": "Gryffindor", "patronus": "Jack Russell terrier"},
+ {"nombre": "Draco", "casa": "Slytherin", "patronus": None},
+]
 
-for clave in estudiantes:
-    print(clave, estudiantes[clave], sep=", ") 
+for estudiante in estudiantes:
+    print(estudiante['casa'])
